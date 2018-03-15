8e2af1d19c027c991800c68a9d8b3950f4368526
ServicedevPubl\Servicedev_Publ <hjshin@playwith.co.kr>
2018-03-15 18:59:38 +0900

    로그인 인증 수정
    수정

--- src/components/login/LoginSNS.vue
+++ src/components/login/LoginSNS.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="box-btn">
-    <md-button class="md-raised">GOOGLE</md-button>
-    <md-button class="md-raised">FACEBOOK</md-button>
+    <md-button class="md-raised" v-on:click="google">GOOGLE</md-button>
+    <md-button class="md-raised" v-on:click="facebook">FACEBOOK</md-button>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    google: (event) => {
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    },
+    facebook: (event) => {
+
+    }
+  }
 }
 </script>
 
